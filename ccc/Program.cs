891c705c2a2de4e7371f719231351f973c5f9f2c
Seyed <abbasi.3195@gmail.com>
2025-09-23 02:37:05 +0330

    a stack class

--- ccc/Program.cs
+++ ccc/Program.cs
@@ -1,34 +1,96 @@
-﻿// get number of element
-var n_o_elem = int.Parse(Console.ReadLine());
-//get the array
-var numbers = new int[n_o_elem];
-for (int i = 0; i < n_o_elem; i++)
-{
-    Console.Write($"give me elemenet number{i + 1}: ");
-    numbers[i] = int.Parse(Console.ReadLine());
-}
-var num_freq = new Dictionary<int, int> { };
+﻿using System.Collections.Generic;
+using System.Text;
 
-foreach (var key in numbers)
+var st1 = new MStack(6);
+st1.Push(1);
+st1.Push(66);
+st1.Push(55556);
+st1.Push(66);
+st1.Push(6);
+st1.Push(500);
+
+Console.WriteLine(st1.ToString());
+st1.sort_ascending();
+Console.WriteLine(st1.ToString());
+class MStack
 {
-    //if doesn't keys.contain count
-    if (!num_freq.ContainsKey(key))
+    private int[] stack;
+    private int top_index = -1;
+
+    //cons
+    public MStack(int size)
     {
-        int count = 0;
-        foreach (var num in numbers)
+        stack = new int[size];
+    }
+    //push
+    public void Push(int num)
+    {
+        // full write msg
+        if (top_index == stack.Length) Console.WriteLine("It's full bro!");
+        //else push
+        else
         {
-            if (num == key)
-            {
-                count++;
-            }
+            top_index++;
+            stack[top_index] = num;
         }
-        num_freq[key] = count;
+    }
+    //pop
+    public int Pop()
+    {
+        if (top_index == -1) { Console.WriteLine("It's empty bro!"); return 0; }
+        else
+        {
+            top_index--;
+            return stack[top_index + 1];
+        }
+    }
+    //peek
+    public int Peek()
+    {
+        if (top_index == -1) { Console.WriteLine("It's empty bro!"); return 0; }
+        else
+        {
+            return stack[top_index];
+        }
+    }
+    //sort_ascending
+    public void sort_ascending()
+    {
+        var temp_list = new List<int> { };
+        // if not empty
+        //pour them into a list
+        for (int i = 0; i < top_index + 1; i++)
+        {
+            temp_list.Add(stack[i]);
+        }
+        //sort
+        temp_list.Sort();
+        temp_list.Reverse();
+        //pour them back
+        for (int i = 0; i < top_index + 1; i++)
+        {
+            stack[i] = temp_list[i];
+        }
+    }
+    //+override
+    public static MStack operator +(MStack a, MStack b)
+    {
+        return new MStack(2);
+    }
+    //ToString override
+    public override string ToString()
+    {
+        var result = new StringBuilder();
+        // var int_list = new List<int> { };
+        for (int i = 0; i < top_index + 1; i++)
+        {
+            result.Append(stack[i]);
+            result.Append(" ,");
+        }
+        return result.ToString();
     }
 }
-foreach (var nf in num_freq)
-{
-    Console.WriteLine($"{nf}");
-}
+
 
 
 
