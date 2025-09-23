1c96056d6bd6b6bf91eeef4e3f3cd609914208a7
Seyed <abbasi.3195@gmail.com>
2025-09-23 16:16:32 +0330

    Heap sort

--- ccc/Program.cs
+++ ccc/Program.cs
@@ -1,95 +1,51 @@
-﻿using System.Collections.Generic;
-using System.Text;
-
-var st1 = new MStack(6);
-st1.Push(1);
-st1.Push(66);
-st1.Push(55556);
-st1.Push(66);
-st1.Push(6);
-st1.Push(500);
-
-Console.WriteLine(st1.ToString());
-st1.sort_ascending();
-Console.WriteLine(st1.ToString());
-class MStack
+﻿//have a list 
+var mlist = new List<int> { 2, 111, 0, -10, 68, 8, 9, 13, 13, 13 };
+var sorted_list = new List<int> { };
+//problem here
+heapify_all(mlist);
+do
 {
-    private int[] stack;
-    private int top_index = -1;
+    Swap(mlist, 0, mlist.Count - 1);
+    sorted_list.Add(mlist[mlist.Count - 1]);
+    mlist.RemoveAt(mlist.Count - 1);
+    heapify_all(mlist);
+    //repeat until mlist.count==1
+} while (mlist.Count > 0);
+//write the sorted list
+foreach (var item in sorted_list)
+{
+    Console.Write($"{item},");
+}
 
-    //cons
-    public MStack(int size)
+void heapify_all(List<int> list)
+{
+    for (int i = list.Count - 1; i > 0; i--)
     {
-        stack = new int[size];
-    }
-    //push
-    public void Push(int num)
-    {
-        // full write msg
-        if (top_index == stack.Length) Console.WriteLine("It's full bro!");
-        //else push
-        else
-        {
-            top_index++;
-            stack[top_index] = num;
-        }
-    }
-    //pop
-    public int Pop()
-    {
-        if (top_index == -1) { Console.WriteLine("It's empty bro!"); return 0; }
-        else
-        {
-            top_index--;
-            return stack[top_index + 1];
-        }
-    }
-    //peek
-    public int Peek()
-    {
-        if (top_index == -1) { Console.WriteLine("It's empty bro!"); return 0; }
-        else
-        {
-            return stack[top_index];
-        }
-    }
-    //sort_ascending
-    public void sort_ascending()
-    {
-        var temp_list = new List<int> { };
-        // if not empty
-        //pour them into a list
-        for (int i = 0; i < top_index + 1; i++)
-        {
-            temp_list.Add(stack[i]);
-        }
-        //sort
-        temp_list.Sort();
-        temp_list.Reverse();
-        //pour them back
-        for (int i = 0; i < top_index + 1; i++)
-        {
-            stack[i] = temp_list[i];
-        }
-    }
-    //+override
-    public static MStack operator +(MStack a, MStack b)
-    {
-        return new MStack(2);
-    }
-    //ToString override
-    public override string ToString()
-    {
-        var result = new StringBuilder();
-        // var int_list = new List<int> { };
-        for (int i = 0; i < top_index + 1; i++)
-        {
-            result.Append(stack[i]);
-            result.Append(" ,");
-        }
-        return result.ToString();
+        heapify(list, i);
     }
 }
+
+void heapify(List<int> list, int i)
+{
+    //if i==0 return
+    if (i == 0) return;
+    int parent = (i - 1) / 2;
+    //if parent<child -> swap and heapify parent
+    if (list[parent] < list[i])
+    {
+        Swap(list, parent, i);
+        heapify(list, parent);
+    }
+}
+
+void Swap(List<int> li, int a, int b)
+{
+    int temp;
+    temp = li[b];
+    li[b] = li[a];
+    li[a] = temp;
+}
+
 
 
 
