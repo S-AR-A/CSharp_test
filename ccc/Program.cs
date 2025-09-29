23eed0b62d30df761ee56bce88714a46a6b964f2
Seyed <abbasi.3195@gmail.com>
2025-09-29 07:42:31 +0330

    regex1

--- ccc/Program.cs
+++ ccc/Program.cs
@@ -1,62 +1,20 @@
-﻿//have a list 
-var mlist = new List<int> { 2, 111, 0, -10, 68, 8, 9, 13, 13, 13 };
-var sorted_list = new List<int> { };
-//problem here
-heapify_all(mlist);
-do
+﻿using System;
+using System.Text.RegularExpressions;
+
+//get input
+string input = Console.ReadLine();
+
+string pattern = @"\b\w+#?(-\w+)*\b";
+//match with regex
+var matches = Regex.Matches(input, pattern);
+//calc the mean
+int i = 0;
+int sum = 0;
+foreach (var match in matches)
 {
-    Swap(mlist, 0, mlist.Count - 1);
-    sorted_list.Add(mlist[mlist.Count - 1]);
-    mlist.RemoveAt(mlist.Count - 1);
-    heapify_all(mlist);
-    //repeat until mlist.count==1
-} while (mlist.Count > 0);
-//write the sorted list
-foreach (var item in sorted_list)
-{
-    Console.Write($"{item},");
+    sum += match.ToString().Length;
+    i++;
 }
-
-void heapify_all(List<int> list)
-{
-    for (int i = list.Count - 1; i > 0; i--)
-    {
-        heapify(list, i);
-    }
-}
-
-void heapify(List<int> list, int i)
-{
-    //if i==0 return
-    if (i == 0) return;
-    int parent = (i - 1) / 2;
-    //if parent<child -> swap and heapify parent
-    if (list[parent] < list[i])
-    {
-        Swap(list, parent, i);
-        heapify(list, parent);
-    }
-}
-
-void Swap(List<int> li, int a, int b)
-{
-    int temp;
-    temp = li[b];
-    li[b] = li[a];
-    li[a] = temp;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+double result = (double)sum / (double)i;
+//output the result
+Console.WriteLine($"{Math.Ceiling(result*100)/100}");
