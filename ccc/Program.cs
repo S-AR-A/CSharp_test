31254706759db225f743bcdf932f7858ef9f1f12
Seyed <abbasi.3195@gmail.com>
2025-10-02 08:50:30 +0330

    datetime practice

--- ccc/Program.cs
+++ ccc/Program.cs
@@ -1,20 +1,10 @@
 ﻿using System;
+using System.Runtime.Intrinsics.Arm;
 using System.Text.RegularExpressions;
 
-//get input
-string input = Console.ReadLine();
-
-string pattern = @"\b\w+#?(-\w+)*\b";
-//match with regex
-var matches = Regex.Matches(input, pattern);
-//calc the mean
-int i = 0;
-int sum = 0;
-foreach (var match in matches)
-{
-    sum += match.ToString().Length;
-    i++;
-}
-double result = (double)sum / (double)i;
-//output the result
-Console.WriteLine($"{Math.Ceiling(result*100)/100}");
+var dt = new DateTime(1300, 02, 04, 12, 11, 10);
+System.Console.WriteLine(dt);
+double number = double.Parse(Console.ReadLine());
+var ts = new TimeSpan(0,0,(int)(number*3600));
+dt=dt.Add(ts);
+System.Console.WriteLine(dt);
