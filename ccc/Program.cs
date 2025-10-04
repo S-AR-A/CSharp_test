c62b20016d5d54207f1959126b28aa51c7efed82
Seyed <abbasi.3195@gmail.com>
2025-10-04 05:04:56 +0330

    Appointment

--- ccc/Program.cs
+++ ccc/Program.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Runtime.Intrinsics.Arm;
-using System.Text.RegularExpressions;
+﻿sing System.Reflection.Metadata.Ecma335;
 
-var dt = new DateTime(1300, 02, 04, 12, 11, 10);
-System.Console.WriteLine(dt);
-double number = double.Parse(Console.ReadLine());
-var ts = new TimeSpan(0,0,(int)(number*3600));
-dt=dt.Add(ts);
-System.Console.WriteLine(dt);
+System.Console.WriteLine
+(
+    Appointment.AnniversaryDate()
+);
+class Appointment()
+{
+    public DateTime Datetime { get; set; }
+    public static DateTime Schedule(string s) => DateTime.Parse(s);
+    public static Boolean HasPassed(DateTime dt)
+    {
+        if (DateTime.Now.CompareTo(dt) > 0) return true;
+        else return false;
+    }
+    public static bool IsAfternoonAppointment(DateTime dt)
+    => (dt.Hour < 18 && dt.Hour >= 12);
+    public static string Description(DateTime dt)
+        => $"You have an appointment on {dt}";
+
+    public static DateTime AnniversaryDate()
+    => new(DateTime.Now.Year, 9, 15, 0, 0, 0);
+    
+}
